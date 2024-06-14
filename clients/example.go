d990384fe4758ebc14b0bc2af5f284ae5a2615b6
chengyinkai <chengyinkai@bytedance.com>
2024-06-14 23:53:15 +0800

    增加swagger功能

--- clients/example.go
+++ clients/example.go
@@ -10,3 +10,16 @@ func MyTest(ctx *gin.Context) {
 	ctx.JSON(http.StatusOK, "ok")
 	return
 }
+
+// @Summary ping example
+// @Schemes
+// @Description 测试的接口
+// @Tags myTest
+// @Accept json
+// @Produce json
+// @Success 200 {string} MyTest
+// @Router /ping [get]
+func Ping(ctx *gin.Context) {
+	ctx.JSON(http.StatusOK, "pong")
+	return
+}
--- routes/route.go
+++ routes/route.go
@@ -3,11 +3,26 @@ package routes
 import (
 	"myProj/clients"
 
+	docs "myProj/docs"
+
 	"github.com/gin-gonic/gin"
+	swaggerfiles "github.com/swaggo/files"
+	ginSwagger "github.com/swaggo/gin-swagger"
 )
 
 func SetUpRoutes(r *gin.Engine) *gin.Engine {
-	r.Group("api/v1")
-	r.GET("test", clients.MyTest)
+	docs.SwaggerInfo.Title = "myProj API"
+	docs.SwaggerInfo.Description = "myProj后端api文档服务"
+	docs.SwaggerInfo.Version = "1.0"
+	docs.SwaggerInfo.BasePath = "/api/v1"
+	docs.SwaggerInfo.Schemes = []string{"http", "https"}
+	r.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerfiles.Handler))
+
+	test := r.Group("api/test")
+	test.GET("tmptest", clients.MyTest)
+
+	v1 := r.Group("api/v1")
+	v1.GET("ping", clients.Ping)
+
 	return r
 }
